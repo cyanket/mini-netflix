94215a05bb1329e7ec087b62e62dceb5189f829f
Sanket <sanketk092@gmail.com>
2025-07-14 21:43:51 +0530

    updated nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Mini Netflix',
+      meta: [
+        { name: 'description', content: 'A simple movie browsing app' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/netflix-logo.svg' },
+      ],
+    },
+  },
   modules: ['@nuxtjs/tailwindcss'],
   runtimeConfig: {
     public: {
